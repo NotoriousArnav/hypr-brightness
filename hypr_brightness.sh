22900f47451c7cd763fb57134d28a16e0c934b0f
Arnav Ghosh <arnavghosh.bca.23@sittechno.edu.in>
2025-04-13 01:22:03 +0530

    Added Auto Dependency Installer

--- hypr_brightness.sh
+++ hypr_brightness.sh
@@ -3,6 +3,44 @@ set +e
 
 usage="Usage: $0 [+] or [-]"
 
+# Check Dependencies and update the flag
+flag=0
+
+if ! command -v jq &> /dev/null
+then
+  flag=1
+fi
+
+if ! command -v ddcutil &> /dev/null
+then
+  flag=1
+fi
+
+if ! command -v brillo &> /dev/null
+then
+  flag=1
+fi
+
+if ! command -v hyprctl &> /dev/null
+then
+  flag=1
+fi
+
+if [ $flag -eq 1 ]; then
+  # Check if Arch linux
+  if [ -f /etc/os-release ]; then
+    if grep -q "ID=arch" /etc/os-release; then
+      # Check if yay or any other AUR helper is installed
+      if command -v yay &> /dev/null; then
+        yay -S jq ddcutil brillo hyprland
+      else
+        echo "yay is not installed"
+        exit 1
+      fi
+    fi
+  fi
+fi
+
 if [ "$#" -ne 1 ]; then
     echo "No direction parameter provided"
     echo "$usage"
@@ -25,7 +63,7 @@ fi
 direction=$arg
 
 monitor_data=$(hyprctl monitors -j)
-focused_name=$(echo $monitor_data | jq -r '.[] | select(.focused == true) | .name')
+focused_name=$(echo $monitor_data | jq -r '.[] | select(.focused == true) | .name' | tr -d '\n')
 
 if [ "$focused_name" == "eDP-1" ]; then
     if [ "$direction" == "-" ]; then
